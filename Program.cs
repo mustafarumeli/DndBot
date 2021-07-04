533fd36c575346f8f2e986b31a9cf64de5d7abe7
Mustafa Rumeli <mustafarumeli@outlook.com>
2021-07-04 21:53:11 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using DndBot.Bot;
+using DndBot.Commands;
+using DndBot.DAL;
+using DndBot.Entities;
+using MongoORM4NetCore;
+using System;
+using System.Threading.Tasks;
+
+namespace DndBot
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            CrudFactory.OpenConnection(databaseName: "botDb");
+            var customBot = new CustomBot();
+            await customBot.RunBotAsync("dndBot", new GeneralCommandGroup(), new CombatCommandGroup(), new NoteCommandGroup());
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
